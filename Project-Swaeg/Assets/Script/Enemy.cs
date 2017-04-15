7983a084c69de1f7ba8ed80a4ffa6c62a620783c
Ira Javier <c15362901@mydit.ie>
2017-04-15 16:45:23 +0100

    enemy shcript huahuaehuda

--- Project-Swaeg/Assets/Script/Enemy.cs
+++ Project-Swaeg/Assets/Script/Enemy.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    Transform tr_Player;
+    float f_RotSpeed = 3.0f, f_MoveSpeed = 3.0f;
+
+    // Use this for initialization
+    void Start () {
+        tr_Player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+        transform.rotation = Quaternion.Slerp(transform.rotation
+                            ,Quaternion.LookRotation(tr_Player.position - transform.position)
+                            ,f_RotSpeed * Time.deltaTime);
+
+        transform.position += transform.forward * f_MoveSpeed * Time.deltaTime;
+
+
+    }
 }
+
+
