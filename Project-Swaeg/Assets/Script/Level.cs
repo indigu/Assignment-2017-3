0f40eabb11b9a8703fc2ef7d6f4f69f4b7abf2da
Erika Secillano <C15339871@mydit.ie>
2017-04-19 02:16:01 +0000

    START BUTTON NOW WORKS :cry:

--- Project-Swaeg/Assets/Script/Level.cs
+++ Project-Swaeg/Assets/Script/Level.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class Level : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public void LoadScene(string name)
+    {
+        Application.LoadLevel(name);
+    }
 }
