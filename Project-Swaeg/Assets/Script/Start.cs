2137671311148dce452e57a95a947381b9a13d92
Erika Secillano <C15339871@mydit.ie>
2017-04-19 01:54:36 +0000

    new scene for the menu :sake:

---
+++ Project-Swaeg/Assets/Script/Start.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Start : MonoBehaviour {
+    public void StartGame(string name)
+    {
+        Application.LoadLevel(name);
+    }
+	
+}
