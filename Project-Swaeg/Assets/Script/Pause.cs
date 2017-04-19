f10e7b11bdb99b6674f68b832e86cde5f8d0a4a2
Erika Secillano <C15339871@mydit.ie>
2017-04-19 00:19:35 +0000

    Code to actually pause the surroundings and resume it
    :wind_chime:  :wink:

--- Project-Swaeg/Assets/Script/Pause.cs
+++ Project-Swaeg/Assets/Script/Pause.cs
@@ -18,10 +18,12 @@ public class Pause : MonoBehaviour {
             if (canvas.gameObject.activeInHierarchy == false)
             {
                 canvas.gameObject.SetActive(true);
+                Time.timeScale = 0;
             }
             else
             {
                 canvas.gameObject.SetActive(false);
+                Time.timeScale = 1;
             }
         }
 	}
