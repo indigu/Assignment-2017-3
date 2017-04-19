e6117560a4c9229eef8a69b81f4040ff2d5c4216
Erika Secillano <C15339871@mydit.ie>
2017-04-19 00:16:36 +0000

    Code to press esc and pause menu appears

---
+++ Project-Swaeg/Assets/Script/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+    public Transform canvas;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (canvas.gameObject.activeInHierarchy == false)
+            {
+                canvas.gameObject.SetActive(true);
+            }
+            else
+            {
+                canvas.gameObject.SetActive(false);
+            }
+        }
+	}
+}
