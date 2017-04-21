9fac623e9da68f64b033f78626c6c8dba1838486
Ira Javier <c15362901@mydit.ie>
2017-04-21 01:25:36 +0100

    reducing health bar attempt hehe xd

--- Project-Swaeg/Assets/Script/HealthBar.cs
+++ Project-Swaeg/Assets/Script/HealthBar.cs
@@ -1,16 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class HealthBar : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
+    // Use this for initialization
+
+    public static int health = 100;
+    public GameObject player;
+    public Slider healthBar;
+
+
+    void Start ()
+    {
+        InvokeRepeating("ReduceHealth", 1, 1);
+    }
+
+	void ReduceHealth()
+    {
+        health = health - 10;
+        healthBar.value = health;
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
 }
+
---
+++ Project-Swaeg/Assets/Script/ReduceHealth.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReduceHealth : MonoBehaviour {
+
+    void onTriggerEnter(Enemy other)
+    {
+        HealthBar.health -= 50;
+    }
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
