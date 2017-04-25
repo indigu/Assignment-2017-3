2a52f709e4a5b5c11081574bf540a392934a54fd
Aries Ogaco <c15477298@mydit.ie>
2017-04-26 00:20:20 +0100

    Got time to work
    timer script working, but too many floats, needs a fix soon, but i gotta
    :poop:  hehe xd

--- Project-Swaeg/Assets/Timer.cs
+++ Project-Swaeg/Assets/Timer.cs
@@ -6,14 +6,20 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour {
 
     public Text timerText;
+    private float startTime;
 
 	// Use this for initialization
 	void Start () {
-		
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        float t = Time.time - startTime;
+
+        string minutes = ((int)t / 60).ToString();
+        string seconds = (t % 60).ToString();
+
+        timerText.text = minutes + ":" + seconds;
 	}
 }
