a91d5b5d72efbd0338d482faa2dbb2985cbe2492
Aries Ogaco <c15477298@mydit.ie>
2017-04-26 01:08:17 +0100

    Creating a bat spawner
    Testing it out with bats, just a static spawner to ensure there's always
    bats in the game

---
+++ Project-Swaeg/Assets/BatSpawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatSpawner : MonoBehaviour {
+
+    public GameObject spawn;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(spawn, transform.position, Quaternion.identity);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
